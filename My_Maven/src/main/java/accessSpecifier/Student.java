2754a0ccc56ed64fc0440962bde0affb8913309e
Dell <Dell@DELL-PC.lan>
2024-08-12 14:53:25 +0530

    Commit todays Work

---
+++ My_Maven/src/main/java/accessSpecifier/Student.java
@@ -0,0 +1,13 @@
+package accessSpecifier;
+
+public class Student {
+	
+	public void display()
+	
+	{
+		System.out.println("Hi World");
+	}
+	
+//public String name="Alby";
+//public int age=25;
+}
---
+++ My_Maven/src/main/java/accessSpecifier/StudentTest.java
@@ -0,0 +1,15 @@
+package accessSpecifier;
+
+public class StudentTest extends Student {
+	
+
+	public static void main(String[] args) {
+		
+		Student obj=new Student();
+		
+		obj.display();
+		
+
+	}
+
+}
